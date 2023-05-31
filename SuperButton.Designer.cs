459c07bd21c6dee9da676ba4ced3cb1792d15ea1
yaguang.chen <yaguang.chen@capstones.cn>
2023-05-31 11:53:18 +0800

    添加SuperButton

---
+++ SuperButton.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Tetris
+{
+    partial class SuperButton
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label = new Label();
+            SuspendLayout();
+            // 
+            // label
+            // 
+            label.BackColor = Color.Transparent;
+            label.Location = new Point(0, 0);
+            label.Name = "label";
+            label.Size = new Size(80, 30);
+            label.TabIndex = 0;
+            label.Text = "Button";
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // SuperButton
+            // 
+            AutoScaleDimensions = new SizeF(6F, 14F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label);
+            Font = new Font("Kleptocracy Titling Rg", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "SuperButton";
+            Size = new Size(80, 30);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label;
+    }
+}
---
+++ SuperButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+namespace Tetris
+{
+    public partial class SuperButton : UserControl
+    {
+        //public Image PressedBackgroundImage { get; set; }
+        //private Image originalBackgroundImage;
+
+        public SuperButton()
+        {
+            InitializeComponent();
+            //MouseDown += OnMouseDown;
+            //MouseUp += OnMouseUp;
+        }
+
+        //private void OnMouseDown(object sender, MouseEventArgs e)
+        //{
+        //    if (BackgroundImage != null && PressedBackgroundImage != null)
+        //    {
+        //        originalBackgroundImage = BackgroundImage;
+        //        BackgroundImage = PressedBackgroundImage;
+        //    }
+        //}
+
+        //private void OnMouseUp(object sender, MouseEventArgs e)
+        //{
+        //    if (BackgroundImage != null && PressedBackgroundImage != null)
+        //    {
+        //        BackgroundImage = originalBackgroundImage;
+        //    }
+        //}
+    }
+}
